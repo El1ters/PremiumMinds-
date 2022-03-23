de78bbc95350e83f66ce21509d9159b973165c52
Miguel Lopes <82033110+El1ters@users.noreply.github.com>
2022-03-23 20:00:13 +0000

    Add files via upload

--- PremiumMind.c
+++ PremiumMind.c
@@ -1,8 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-unsigned char string [100000001];
-void CompareString(char letter,int *k);
+void CompareString(char letter,int *k,char *string);
 void Read();
 
 int main(){
@@ -10,7 +9,7 @@ int main(){
     return 0;
 }
 
-void CompareString(char letter,int *k){
+void CompareString(char letter,int *k,char *string){
     if((string[(*k) - 1] - letter == 17) || (string[(*k) - 1] - letter == -17)){
         string[(*k)--] = '\0';
         return;
@@ -29,6 +28,7 @@ void Read(){
     int i = 0;
     int k = 0;
     char number[9];
+    char *string;
     while (letter != '\n'){
         letter = getc(stdin);
         if(aux == 0){
@@ -36,7 +36,9 @@ void Read(){
                 number[i] = letter;
             }else{
                 number_of_letters = atoi(number);
+                string = malloc(sizeof(char) * number_of_letters);
                 if(number_of_letters == 0){
+                    free(string);
                     exit(0);
                 }
                 aux = 1;
@@ -47,13 +49,12 @@ void Read(){
                 if(k == 0){
                     string[k++] = letter;
                 }else
-                    CompareString(letter,&k);
+                    CompareString(letter,&k,string);
                 number_of_letters--;
             }
         }
         i++;
     }
     printf("%s",string);
+    free(string);
 }
-
-
